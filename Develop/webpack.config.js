a3ca8b40dd23a9e09d8b9a0e365afd8dfafe2be1
Muhammad Anas <muhammadanas112@gmail.com>
2020-12-16 19:36:39 -0500

    new

---
+++ Develop/webpack.config.js
@@ -0,0 +1,9 @@
+const config = {
+    entry: "./src/transaction.js",
+    output: {
+        path: ___dirname + "/dist",
+        filename: "index.js"
+    },
+    mode: "development",
+}
+module.exports = config;
