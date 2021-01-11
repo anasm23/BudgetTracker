d011463a15110143b559435224549d5fa9d9770c
Muhammad Anas <muhammadanas112@gmail.com>
2021-01-11 11:42:14 -0500

    m

--- models/transaction.js
+++ models/transaction.js
@@ -7,11 +7,11 @@ const transactionSchema = new Schema(
     name: {
       type: String,
       trim: true,
-      required: "Enter a name for transaction"
+      required: "Please enter a name for transaction"
     },
     value: {
       type: Number,
-      required: "Enter an amount"
+      required: "Please enter an amount"
     },
     date: {
       type: Date,
